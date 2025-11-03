53fe3baceb1027e131966b28af9bef52e386f1be
Abhijit Singh <abhijitsingh1718@gmail.com>
2025-11-04 02:40:42 +0530

    Intital Commit

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+#1. Setup FastAPI
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+from ai_agent import graph, SYSTEM_PROMPT, parse_response
+
+
+app = FastAPI()
+
+
+#2. Receive and validate request from frontend
+class Query(BaseModel):
+    message: str
+
+@app.post("/ask")
+async def ask(query: Query):
+    inputs = {"messages": [("system", SYSTEM_PROMPT), ("user", query.message)]}
+    #inputs = {"messages": [("user", query.message)]}
+    stream = graph.stream(inputs, stream_mode="updates")
+    tool_called_name, final_response = parse_response(stream)
+
+    # Step3: Send response to the frontend
+    return {"response": final_response,
+            "tool_called": tool_called_name}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
